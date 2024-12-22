1c92b80ca2220f75122408991c44f1d305ee2509
ef-code <no-reply@github.com>
2024-12-22 19:41:06 +0100

    Added a nested dictionary with multiple languages and words pairs

--- gui_dictionary.py
+++ gui_dictionary.py
@@ -1,1 +1,17 @@
 #This program translates words in the specified languages to English
+
+# dictionary with some words and translations
+dictionary = {
+    "French": {
+        "bonjour": "good morning", "merci": "thank you", "au revoir": "goodbye"
+    },
+    "Spanish": {
+        "hola": "hello", "gracias": "thank you", "adios": "goodbye"
+    },
+    "Hausa": { "sannu": "hello", "nagode": "thank you", "zo": "come"
+    },
+    "Yoruba": {"wa": "come", "iwo": "you", "oruko": "oruko"
+    },
+    "Igbo": { "aloo": "hello", "daalu": "thank you", "bia": "come"
+    }
+}
